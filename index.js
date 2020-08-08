6b0ff3af56eef4533d72f054bb9d28d4916f4cb6
arielbk <buchwald.kearney@gmail.com>
2020-08-08 20:54:59 +0200

    Basic messaging working

---
+++ index.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when a client connects
+io.on('connection', socket => {
+  // Welcome current user
+  socket.emit('message', 'Welcome to the chat');
+
+  // broadcast when a user connects
+  // _broadcast_ goes out to everyone but the the user that initiates it
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  // runs when client disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  });
+
+  // listen for chat message
+  socket.on('chatMessage', msg => {
+    io.emit('message', msg);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,38 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+
 const socket = io();
 
+// message received from server
 socket.on('message', message => {
   console.log(message);
+  outputMessage(message);
+
+  // scroll to bottom
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
+
+// message submit
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get message text
+  const msg = e.target.elements.msg.value;
+
+  // emit message to server
+  socket.emit('chatMessage', msg);
+
+  // clear input
+  e.target.elements.msg.value = '';
+});
+
+// output message to dom
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<p class="meta">Mary <span>9:15pm</span></p>
+  <p class="text">
+    ${message}
+  </p>`;
+  document.querySelector('.chat-messages').appendChild(div);
+};
